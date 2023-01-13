0bdfcf17f0119aee66df7961708336e30abae749
Xenor33 <Nightinlatvia33@outlook.com>
2023-01-13 01:27:33 +0100

    added 7nak

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ class Game:
 	def __init__(self):
 
 		# game attributes
-		self.max_level = 2 # we set the number of available levels at 0
+		self.max_level = 5 # we set the number of available levels at 0
 		self.max_health = 100
 		self.cur_health = 100
 		self.coins = 0
